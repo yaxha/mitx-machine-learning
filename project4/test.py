9fcdf616faf5ffed21477a80b86445fa4b626bd0
José Barrios <jose-bar@hotmail.com>
2021-04-20 21:11:07 -0600

    RMSE for K=12

--- project4/test.py
+++ project4/test.py
@@ -2,11 +2,22 @@ import numpy as np
 import em
 import common
 
-X = np.loadtxt("test_incomplete.txt")
-X_gold = np.loadtxt("test_complete.txt")
+X = np.loadtxt("netflix_incomplete.txt")
+X_gold = np.loadtxt("netflix_complete.txt")
 
-K = 4
-n, d = X.shape
-seed = 0
+K = 12
 
-# TODO: Your code here
+log_lh = [0, 0, 0, 0, 0]
+best_seed = 0
+mixtures = [0, 0, 0, 0, 0]
+posts = [0, 0, 0, 0, 0]
+rmse = 0.
+
+# Test all seeds
+for i in range(5):
+    mixtures[i], posts[i], log_lh[i] = em.run(X, *common.init(X, K, i))
+
+best_seed = np.argmax(log_lh)
+Y = em.fill_matrix(X, mixtures[best_seed])
+rmse = common.rmse(X_gold, Y)
+print("RMSE for K = 12: {:.4f}".format(rmse))
